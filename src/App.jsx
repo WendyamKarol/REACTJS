1967ea6ba775c906a8817418c5014b6d43d949c8
WendyamKarol <karolnaze75@gmail.com>
2025-01-08 11:08:18 +0100

    part : syntaxe jsx end

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
 const title = 'Interpolation' 
 const style = {color : 'red', backgroundColor: 'grey'}
 const showTitle = true
+const todos = [
+  'Présenter React',
+  'Présenter le JSX',
+  'Créer des composants'
+]
 
 function App() {
 
   return <>
-      {showTitle && <h1 id="title" className="title" style={style}>{title}</h1>}
+      <Title color ="Red">Mon Titre</Title>
       <p>
+        <input type="text"/>
         En utilisant, l'Interpolation, j'ai la main sur le titre en variable glogbale, au lieu d'entrer directement dans le code.
-        C'est plus pratique et efficace. Très rapide le truc.
+        C'est plus pratique et efficiace. Très rapide le truc.
       </p>
+      <ul>
+        {todos.map(todo => (<li key={todo}>{todo}</li>))} 
+      </ul>
   </>
 
 }
 
-export default App
+const props ={
+  id :'Samir',
+  className : 'maclass'
+}
+
+
+function Title({color,children,hidden}){
+  if (hidden){
+    return null
+  }
+  return <h1 style ={{color : color}}  {...props}>{children}</h1>
+
+}
+
+export default  App
