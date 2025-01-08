dd58e80873e4e09ec7760a0a5d21bb0324fc1933
WendyamKarol <karolnaze75@gmail.com>
2025-01-08 13:08:58 +0100

    Incrementation Hook useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,27 @@
-const title = 'Interpolation' 
-const style = {color : 'red', backgroundColor: 'grey'}
-const showTitle = true
-const todos = [
-  'Présenter React',
-  'Présenter le JSX',
-  'Créer des composants'
-]
+import { useState } from "react"
 
-function App() {
+function App(){
 
+  const [count, SetCount] = useState(0)
+
+
+  const increment = () =>{
+    SetCount(count + 1)
+
+  }
+    
+
+  
   return <>
-      <Title color ="Red">Mon Titre</Title>
-      <p>
-        <input type="text"/>
-        En utilisant, l'Interpolation, j'ai la main sur le titre en variable glogbale, au lieu d'entrer directement dans le code.
-        C'est plus pratique et efficiace. Très rapide le truc.
-      </p>
-      <ul>
-        {todos.map(todo => (<li key={todo}>{todo}</li>))} 
-      </ul>
+
+    <p> Compteur : {count}</p>
+    <button onClick={increment}>Incrémenter</button>
+  
   </>
 
-}
-
-const props ={
-  id :'Samir',
-  className : 'maclass'
-}
 
 
-function Title({color,children,hidden}){
-  if (hidden){
-    return null
-  }
-  return <h1 style ={{color : color}}  {...props}>{children}</h1>
 
 }
 
-export default  App
+export default App
